05a118ca53a9788b32208a3de85e4d498b52f99f
axatrikx <axatrikx@gmail.com>
2016-09-12 17:54:58 +0530

    removed unnecessary statement

--- tests/pages/SearchPage.js
+++ tests/pages/SearchPage.js
@@ -5,7 +5,6 @@ this.SearchPage = $page("Search page", {
     searchbox: "input[name='s']",
 
     doSearch: loggedFunction ("Do Search", function (query){
-        var actions = new Actions(this.driver);
         this.searchbox.typeText(query + Keys.ENTER);
     }) 
 });
